4f5f6c890ef336434ab9e641e0181cd79fee9268
oliverpecha <oliverpecha@gmail.com>
2020-04-19 22:50:03 +0200

    Book / Chapter 8 / Programming Exercise 16 Complete

--- CS106A/Programming Exercises from the Art of Java, Chapter 8 (Strings and Characters)/Acronym.java
+++ CS106A/Programming Exercises from the Art of Java, Chapter 8 (Strings and Characters)/Acronym.java
@@ -1,8 +1,17 @@
 /*
  * File: Acronym.java
- * ------------------
- * This file generates acronyms.
+ * Name: Oliver Pecha
+ * Section Leader: Online Learning
+ * -----------------
+ * Book / Chapter 8 / Programming Exercise 16
+ * -----------------
+ * Rewrite the acronym method described in the section on “Searching within a string” so that it uses the StringTokenizer class to separate 
+ * the words in the argument. This change will allow the acronym to return the correct result even if the words are not separated by spaces. 
+ * As an example, your implementation should be able to form the acronym scuba from the string self-contained underwater breathing apparatus, 
+ * even though the first two words are separated by a hyphen rather than a space.
  */
+
+import java.util.StringTokenizer;
 
 import acm.program.*;
 
@@ -20,14 +29,29 @@ public class Acronym extends ConsoleProgram {
  * Returns the factorial of n, which is defined as the
  * product of all integers from 1 up to n.
  */
+	
 	private String acronym(String str) {
+		String result = "";
+		StringTokenizer strTokenizer = new StringTokenizer(str, DELIMS);
+		
+		while (strTokenizer.hasMoreTokens()) {
+			String currentToken = strTokenizer.nextToken();
+			result += currentToken.substring(0, 1);
+		}
+		return result;
+	}
+	
+	private String oldAcronym(String str) {
 		String result = str.substring(0, 1);
 		int pos = str.indexOf(' ');
 		while (pos != -1) {
+			System.out.println("\npost entry loop" +pos);
 			result += str.substring(pos + 1, pos + 2);
 			pos = str.indexOf(' ', pos + 1);
+			System.out.println("post exit loop" +pos);
 		}
 		return result;
 	}
 
+	private static final String DELIMS = "-: <>@#$%^&*";
 }
