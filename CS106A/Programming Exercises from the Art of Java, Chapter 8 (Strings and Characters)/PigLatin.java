8300010b5038be064ecae91f25aab7e112df1895
oliverpecha <oliverpecha@gmail.com>
2020-04-22 22:37:18 +0200

    Book / Chapter 8 / Programming Exercise 17 & 18 complete

--- CS106A/Programming Exercises from the Art of Java, Chapter 8 (Strings and Characters)/PigLatin.java
+++ CS106A/Programming Exercises from the Art of Java, Chapter 8 (Strings and Characters)/PigLatin.java
@@ -1,16 +1,27 @@
 /*
  * File: PigLatin.java
+ * Name: Oliver Pecha
+ * Section Leader: Online Learning
+ * -----------------
+ * Book / Chapter 8 / Programming Exercise 17 & 18
  * -------------------
- * This file takes a line of text and converts each word
- * into Pig Latin.  The rules for forming Pig Latin words
- * are as follows:
- *
- * o  If the word begins with a vowel, add "way" to the
- *    end of the word.
- *
- * o  If the word begins with a consonant, extract the set
- *    of consonants up to the first vowel, move that set
- *    of consonants to the end of the word, and add "ay".
+ * 17:  Rewrite the PigLatin program in this chapter so that it uses the algorithmic strategy of going through the line character by character 
+ *    	rather than word by word. The pseudocode version of this strategy appears in the section on “Talking spaces and punctuation into account.” 
+ * 18:  As written, the PigLatin program in Figure 8-6 behaves somewhat oddly  if you enter a string that includes words that begin with an 
+ * 		uppercase letter. For example if you were to capitalize the first word in the sentence and the name of the Pig latin language, you would 
+ * 		see the following output:
+ * -------------------
+ * This program translates a line into Pig Latin.
+ * Enter a line: This is Pig Latin.
+ * isThay isway igPay atinLay.
+ * -------------------
+ * 		Rewrite the translateWord method so that any word that begins with a capital letter in the English line still begins with a capital 
+ * 		letter in Pig Latin. Thus, after making your changes in the program, the output should look like this:
+ * -------------------
+ * This program translates a line into Pig Latin. 
+ * Enter a line: This is Pig Latin.
+ * Isthay isway Igpay Atinlay.
+ * -------------------
  */
 
 import acm.program.*;
@@ -19,13 +30,60 @@ import java.util.*;
 public class PigLatin extends ConsoleProgram {
 
 	public void run() {
-		println("This program translates a line into Pig Latin.");
-		String line = readLine("Enter a line: ");
-		println(translateLine(line));
+		while (true) {
+		println("\nThis program translates a line into Pig Latin.");
+		
+		String line = readLine("Enter a line: (This is Pig Latin.) ");
+		println("\"" + translateLine(line) + "\"");
+		}
 	}
 
 /* Translates a line to Pig Latin, word by word */
 	private String translateLine(String line) {
+		// Initialize a variable to hold the result and set it to the empty string.
+		System.out.println("lenght is: " + line.length());
+		String result = "";
+		String extractedWord = "";
+		int StartingChar = 0;
+		for (int i = 0; i < line.length(); i++) {
+			System.out.println("i is: " + i + "StartingChar is: " + StartingChar);
+			// If the ith character in the line is some kind of separator
+			if (isSeparator(line.charAt(i))) {
+				//Append that character to the result variable.
+				result = result + line.charAt(i);
+				System.out.println("if isSeparator, result is: " + result);
+				// If not the last char in the String, reset StartingChar for wordExtractor to trim correctly
+				if (i + 1 < line.length()) {
+				StartingChar = i + 1;
+				} 
+			}
+			// Else If, the ith character is the end of a word
+			else if (i + 1 < line.length()) {	
+				if (isSeparator(line.charAt(i + 1))) {
+					// Extract the word as a substring. 		
+					extractedWord = wordExtractor(line, StartingChar, i + 1);
+					// Translate the word to Pig Latin.	
+					// Append the translated word to the result variable.
+					result = result + translateWord(extractedWord);
+				}
+			}
+			// Else If, the ith character is the end of the String Line entered
+			else if (i + 1 == line.length()) {
+				extractedWord = wordExtractor(line, StartingChar, i + 1);
+				result = result + translateWord(extractedWord);
+			}
+		}
+		// Return the value of the result variable to the caller.
+		return result;
+	}
+	
+	private String wordExtractor(String line, int StartingChar, int i) {
+		String extractedWord = line.substring(StartingChar, i);
+		System.out.println("extractedWord is: \"" + extractedWord + "\"");
+		return extractedWord;
+	}
+/* Translates a line to Pig Latin, word by word */
+	private String translateLineOld(String line) {
 		String result = "";
 		StringTokenizer tokenizer =
 		  new StringTokenizer(line, DELIMITERS, true);
@@ -38,6 +96,17 @@ public class PigLatin extends ConsoleProgram {
 		}
 		return result;
 	}
+	
+	private boolean isSeparator(char currentChar) {
+		boolean result = false;
+		switch (currentChar) {
+		case ',': 
+		case '.': 
+		case '?':
+		case ' ': result = true;
+		}
+		return result;
+	}
 
 /* Returns true if token is a "word" (all character are letters) */
 	private boolean isWord(String token) {
@@ -47,9 +116,42 @@ public class PigLatin extends ConsoleProgram {
 		}
 		return true;
 	}
-
 /* Translates a word to Pig Latin and returns the translated word */
 	private String translateWord(String word) {
+		String result = "";
+		int vp = findFirstVowel(word);
+		if (vp == -1) {
+			if (Character.isUpperCase(word.charAt(0))) {
+				word = capitalizedWord(word);
+			}
+			result = word;
+		} else if (vp == 0) {
+			if (Character.isUpperCase(word.charAt(0))) {
+				word = capitalizedWord(word);
+			}
+			result = word + "way";
+		} else {
+			String head = word.substring(0, vp);
+			String tail = word.substring(vp);
+			if (Character.isUpperCase(word.charAt(0))) {
+				tail = capitalizedWord(tail);
+				head = head.toLowerCase();
+			}
+			result = tail + head + "ay";
+		}
+		return result;
+	}
+	private String capitalizedWord(String word) {
+		word = word.toLowerCase();
+		String capitalized = null;
+		capitalized = "" + word.charAt(0);
+		capitalized = capitalized.toUpperCase();
+		capitalized = capitalized + word.substring(1);
+		return capitalized;
+	}
+	
+/* Translates a word to Pig Latin and returns the translated word */
+	private String translateWordOld(String word) {
 		int vp = findFirstVowel(word);
 		if (vp == -1) {
 			return word;
@@ -82,6 +184,6 @@ public class PigLatin extends ConsoleProgram {
 
 /* Defines the characters that delimit a token */
 	private static final String
-	  DELIMITERS = "!@#$%^&*()_-+={[}]:;\"'<,>.?/~`|\\ ";
+	  DELIMITERS = "!@#$%^&*()_-+={[}]:;\"'<,>.?/~`|\\";
 
 }
