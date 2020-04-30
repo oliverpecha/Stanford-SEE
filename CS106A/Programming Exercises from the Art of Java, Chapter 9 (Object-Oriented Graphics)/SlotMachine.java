cc61173fe139c8042c0a5892d6bc07df308be6c5
oliverpecha <oliverpecha@gmail.com>
2020-04-30 23:03:04 +0200

    Book / Chapter 9 / Programming Exercise 13 complete

---
+++ CS106A/Programming Exercises from the Art of Java, Chapter 9 (Object-Oriented Graphics)/SlotMachine.java
@@ -0,0 +1,175 @@
+/*
+ * File: SlotMachine.java
+ * Name: Oliver Pecha
+ * Section Leader: Online Learning
+ * -----------------
+ * Book / Chapter 9 / Programming Exercise 13
+ * -----------------
+ * In casinos from Monte Carlo to Las Vegas, one of the most common gambling devices is the slot machine - the “one-armed bandit.” A typical 
+ * slot machine has three wheels that spin around behind a  narrow window. Each wheel is marked with the following symbols: 
+ * CHERRY, LEMON, ORANGE, PLUM, BELL, BAR. The window, however, allows you to see only one symbol on each wheel at a time. 
+ * For example, the window might show the following configuration:
+ * If you put a silver dollar onto a slot machine and pull the handle on its side, the wheels pin around and eventually come to rest in some 
+ * new configuration. If the configuration matches one of a set of winning patterns printed on the front of the slot machine, you get back 
+ * some money. If not, you’re out a dollar. The following table shows a typical set of winning patterns, along with their associated payoffs:
+ * -----------------	
+ * 		BAR		BAR		BAR 		pays	$250
+ * 		BELL 	BELL 	BELL/BAR 	pays	$20
+ * 		PLUM	PLUM	PLUM/BAR	pays	$14
+ * 		ORANGE	ORANGE	ORANGE/BAR	pays	$10
+ * 		CHERRY	CHERRY	CHERRY		pays	$7
+ * 		CHERRY	CHERRY	-			pays	$5
+ * 		CHERRY	-		-			pays	$2
+ * -----------------
+ * The notation BELL/BAR means that either a BELL or a BAR can appear in that position, and the dash means that any symbol at all can appear. 
+ * This, getting a CHERRY in the first position is automatically good for two dollars, no matter what appears on the other wheels. 
+ * The LEMON symbol never pays off, even if you happen to line up three of them.
+ * Write a program that simulates playing a slot machine. Your program should provide the user decides to quit. During each round, your program 
+ * should take away a dollar, simulate the spinning of the wheels, evaluate the result, and pay the user an appropriate winnings. For example, 
+ * a user might be lucky enough to see the following sample run:
+ * -----------------
+ * Even though doing so is not realistic (and would make the slot machine unprofitable for the casino), you should assume that each of the six 
+ * symbols is equally on each wheel.
+ */
+
+import acm.program.*;
+import acm.graphics.*;
+import acm.util.RandomGenerator;
+import java.awt.Color;
+
+public class SlotMachine extends GraphicsProgram {
+
+	private RandomGenerator luck = RandomGenerator.getInstance();
+
+	public void init() {
+		setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+	}
+	
+	public void run() {
+		int balance = INITIAL_BUDGET;
+		/* Graphics */
+		int margin = (SCREEN_WIDTH - SLOT_WIDTH * 3) / 4;
+		GRect background = new GRect(SCREEN_WIDTH, SCREEN_HEIGHT);
+			background.setFilled(true);
+			background.setColor(Color.lightGray);
+			background.setFillColor(Color.lightGray);
+			add(background);	
+		GImage firstSlot = new GImage("empty.gif");
+			firstSlot.setSize(SLOT_WIDTH, SLOT_WIDTH);
+			add(firstSlot, margin, margin);
+		GImage secondSlot = new GImage("empty.gif");
+			secondSlot.setSize(SLOT_WIDTH, SLOT_WIDTH);
+			add(secondSlot, margin * 2 + SLOT_WIDTH, margin);
+		GImage thirdSlot = new GImage("empty.gif");
+			thirdSlot.setSize(SLOT_WIDTH, SLOT_WIDTH);
+			add(thirdSlot, margin * 3 + SLOT_WIDTH * 2, margin);
+		GLabel dialogOne = new GLabel("Welcome to the slot \nmachine!");
+			dialogOne.setFont("-20");
+			add(dialogOne, margin, margin * 2.5 + firstSlot.getHeight());
+		GLabel dialogTwo = new GLabel("You now have $" + balance + ".");
+			dialogTwo.setFont("-20");
+			add(dialogTwo, margin, margin * 3.5 + firstSlot.getHeight());
+		GLabel dialogThree = new GLabel("Click to play.");
+			dialogThree.setFont("-20");
+			add(dialogThree, margin, margin * 4.5 + firstSlot.getHeight());
+			
+		int windowFirst = 0;
+		int windowSecond = 0;
+		int windowThird = 0;
+		int roundBalance = 0;
+		while (balance > COST_TO_PLAY) {	
+			waitForClick();
+			balance -= COST_TO_PLAY;
+			roundBalance = balance;
+			windowFirst = symbolReset();
+			windowSecond = symbolReset();
+			windowThird = symbolReset();
+			balance = symbolConversion(balance, windowFirst, windowSecond, windowThird);
+			pause(PAUSE_SHORT);
+			firstSlot.setImage(toResource(windowFirst));
+			firstSlot.setSize(SLOT_WIDTH, SLOT_WIDTH);
+			pause(PAUSE);
+			secondSlot.setImage(toResource(windowSecond));
+			secondSlot.setSize(SLOT_WIDTH, SLOT_WIDTH);
+			pause(PAUSE_LONG);
+			thirdSlot.setImage(toResource(windowThird));
+			thirdSlot.setSize(SLOT_WIDTH, SLOT_WIDTH);
+			if (balance == roundBalance) dialogOne.setLabel("You loose.");
+			else dialogOne.setLabel("You win $" + (balance - roundBalance) + "."); 
+			dialogTwo.setLabel("You now have $" + balance + ".");
+		}	
+		// user doesn't have enough funds to continue play
+		if (balance < COST_TO_PLAY)
+			dialogTwo.setLabel("You only have $" + balance + ".");
+			dialogThree.setColor(Color.red);
+			dialogThree.setLabel("To play, you need at least $" + COST_TO_PLAY + ".");
+	}
+	
+	/* This method returns a new int representing a symbol. */
+	public int symbolReset() {
+			int dice = luck.nextInt(1,6);
+			return dice;
+	}
+	
+	/* This method handles rules on conversions symbol combination to money. */
+		public int symbolConversion(int balance, int windowFirst, int windowSecond, int windowThird) {
+			 /* 	BAR		BAR		BAR 		pays	$250
+			 * 		BELL 	BELL 	BELL/BAR 	pays	$20
+			 * 		PLUM	PLUM	PLUM/BAR	pays	$14
+			 * 		ORANGE	ORANGE	ORANGE/BAR	pays	$10
+			 * 		CHERRY	CHERRY	CHERRY		pays	$7
+			 * 		CHERRY	CHERRY	-			pays	$5
+			 * 		CHERRY	-		-			pays	$2 */
+			if (windowFirst == 6 && windowSecond == 6 && windowThird == 6) balance += MAX_WIN;
+			if (windowFirst == 5 && windowSecond == 5 && windowThird == 5 || windowThird == 6) balance += 20;
+			if (windowFirst == 4 && windowSecond == 4 && windowThird == 4 || windowThird == 6) balance += 14;
+			if (windowFirst == 3 && windowSecond == 3 && windowThird == 3 || windowThird == 6) balance += 10;
+			if (windowFirst == 1 && windowSecond == 1 && windowThird == 1) balance += 7;
+			if (windowFirst == 1 && windowSecond == 1) balance += 5;
+			if (windowFirst == 1) balance += 2;
+			return balance;
+		}
+	
+		
+	/* This method handles conversion from int to Strings to display Symbols humans can read. */
+		private String toResource(int cases) {
+			String name = null;
+			switch (cases) {
+				case 1: 	name = (CHERRY); break;
+		        case 2: 	name = (LEMON); break;
+		        case 3: 	name = (ORANGE); break;
+		        case 4: 	name = (PLUM); break;
+		        case 5: 	name = (BELL); break;
+		        case 6: 	name = (BAR); break;
+			}
+			return  name;
+		}
+		
+	/* Constants that define rules on budget to play */
+		private static final int INITIAL_BUDGET = 50;
+		private static final int COST_TO_PLAY = 15;
+		private static final int MAX_WIN = 250;
+		
+	/* Constants that contain Strings on Symbols */
+		private static final String CHERRY = "cherry.gif";
+		private static final String LEMON = "lemon.gif";
+		private static final String ORANGE = "orange.gif";
+		private static final String PLUM = "plum.gif";
+		private static final String BELL = "bell.gif";
+		private static final String BAR = "bar.gif";
+
+	/* Constants that contain Screen Size value */
+		private static final int SLOT_WIDTH = 100;
+	
+		/* Constants that contain Screen Size value */
+		private static final int PAUSE_LONG = 900;
+		private static final int PAUSE = 500;
+		private static final int PAUSE_SHORT = 200;
+		
+	/* Constants that contain Screen Size value */
+		private static final int SCREEN_WIDTH = 400;
+		private static final int SCREEN_HEIGHT = 250;
+	
+}
+
+
