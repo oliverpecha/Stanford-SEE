ec9574ad89f3694e6296b71067115f57a23972d1
oliverpecha <oliverpecha@gmail.com>
2020-05-14 22:34:16 +0200

    Book / Chapter 11 / Programming Exercise 1 & 2 complete

--- CS106A/Programming Exercises from the Art of Java, Chapter 11/GymnasticsJudge.java
+++ CS106A/Programming Exercises from the Art of Java, Chapter 11/GymnasticsJudge.java
@@ -3,7 +3,7 @@
  * Name: Oliver Pecha
  * Section Leader: Online Learning
  * -----------------
- * Book / Chapter 11 / Programming Exercise 1
+ * Book / Chapter 11 / Programming Exercise 1 & 2
  * -----------------
  * Because individual judges may have some bias, it is common practice to throw out the highest and lowest score 
  * before computing the average. Write a program that reads in scores from a panel of seven judges and then computes 
@@ -20,16 +20,22 @@ public class GymnasticsJudge extends ConsoleProgram {
 		for (int i = 0; i < nJudges; i++) {
 			scores[i] = readDouble("Score for judge " + (i + 1) + ": ");
 		}
-		double total = 0;
-		double max = scores[0];
-		double min = scores[0];
-		for (int i = 0; i < nJudges; i++) {
-			if (scores[i] < min) min = scores[i];
-			if (scores[i] > max) max = scores[i];
-			total += scores[i];
-		}
-		double averageScore = (total - max - min) / (nJudges - 2);
+		double averageScore =  mean(scores);
 		println("The average score is " + averageScore);
 	}
 
+	private double mean(double [] array) {
+		int lenght = array.length;
+		double total = 0;
+		double max = array[0];
+		double min = array[0];
+		for (int i = 0; i < lenght; i++) {
+			if (array[i] < min) min = array[i];
+			if (array[i] > max) max = array[i];
+			total += array[i];
+		}
+		double mean = (total - max - min) / (lenght - 2);
+		return mean;
+	}
+	
 }
