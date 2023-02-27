01f8194476ca61871f84953ae47f3810b7cc1094
oliverpecha <oliverpecha@gmail.com>
2023-02-27 11:38:11 +0100

    c03e15 done

--- CS106B/Chapter03 (Strings) Book Exercises/14. addCommas/addCommas.cpp
+++ CS106B/Chapter03 (Strings) Book Exercises/14. addCommas/addCommas.cpp
@@ -33,53 +33,25 @@ using namespace std;
 
 string addCommas(string digits);
 
-/*
+
 int main() {
     while (true) {
         string digits;
         cout << "Enter a number: ";
         getline(cin, digits);
         if (digits == "") break;
-        cout << addCommas(digits) << endl;
+        cout << addCommas(digits) << "\n" << endl;
     }
     return 0;
 }
-*/
 
 
-int main() {
-        cout << addCommas("1") << " 1" << endl;
-        cout << "\n" << addCommas("10") << " 2" << endl;
-        cout << "\n" << addCommas("100") << " 3" << endl;
-        cout << "\n" << addCommas("1000") << " 4" << endl;
-        cout << "\n" << addCommas("10000") << " 5" << endl;
-        cout << "\n" << addCommas("100000") << " 6" << endl;
-        cout << "\n" << addCommas("1000000") << " 7" << endl;
-        cout << "\n" << addCommas("10000000") << " 8" << endl;
-        cout << "\n" << addCommas("100000000") << " 9" << endl;
-        cout << "\n" << addCommas("1000000000") << " 10" << endl;
-        cout << "\n" << addCommas("10000000000") << " 11" << endl;
-        cout << "\n" << addCommas("100000000000") << " 12" << endl;
-        cout << "\n" << addCommas("1000000000000") << " 13" << endl;
-
-
-
-
-    return 0;
-}
 
 string addCommas(string digits) {
-
     double x = (double)digits.length() * 1 / 3;
     x = fmod(x,1.0);
-    if (x == 0) {
-        x = digits.length() / 3 - 1;
-        //cout << "reduce" << endl;
-    }
-    else {
-    // cout << "ok" << endl;
-     x = digits.length() / 3;
-    }
+    if (x == 0) x = digits.length() / 3 - 1;
+    else x = digits.length() / 3;
     int ticker = 0;
     if (digits.length() > 3) {
         for (int i = 1; i <= x ; i++) {
